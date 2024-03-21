d7beacb14e44688a17b52e34fcf2aeab22b01860
SiddharthaVarshney <sidkvag@gmail.com>
2024-03-21 19:19:22 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "Siddhartha Varshney";
+const meta = {
+    title: "Siddhartha Varshney",
+    description: "I’m Siddhartha Varshney Full stack devloper, currently studing in VIT Vellore",
+};
+
+const introdata = {
+    title: "I’m Siddhartha Varshney",
+    animated: {
+        first: "I love coding cool websites",
+        second: "I love solving Leetcode problems",
+        third: "I developed cool apps",
+    },
+    description: "I’m Siddhartha Varshney Full stack devloper, currently studing in VIT Vellore",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I’m Siddhartha Varshney Full stack devloper, currently studing in VIT Vellore.",
+};
+const worktimeline = [{
+        jobtitle: "ML Instructor",
+        where: "INTEL India",
+        date: "2023",
+    },
+    {
+        jobtitle: "Web Developer Intern",
+        where: "CDAC Noida",
+        date: "2023",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Texas",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 65,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "FastApi",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Front End",
+        description: "Front-end development involves creating the visual and interactive elements of websites using HTML, CSS, and JavaScript, ensuring a seamless and responsive user experience across different devices and browsers.",
+    },
+    {
+        title: "Back End",
+        description: "Back-end development focuses on server-side logic, databases, and application integration, powering the behind-the-scenes functionality of websites and applications.",
+    },
+    {
+        title: "Machine Learning",
+        description: "Machine learning is a field of artificial intelligence that enables computers to learn and make decisions from data without being explicitly programmed.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "siddhartha.varshney03@gmail.com",
+    YOUR_FONE: "+919811362639",
+    description: "I am available for freelance work. Connect with me via phone or email",
+    // to add emailjs.com account https://www.emailjs.com/docs/examples/reactjs/
+    // YOUR_SERVICE_ID: "service_id",
+    // YOUR_TEMPLATE_ID: "template_id",
+    // YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/SiddharthaVarshney",
+    leetcode: "https://leetcode.com/Sidd30/",
+    linkedin: "https://www.linkedin.com/in/siddhartha-varshney-0b6b64204/",
+    twitter: "https://twitter.com/Sid_3007",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
